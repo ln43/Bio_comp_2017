80d403d3d698dc5f0ba6af5d36eb1034b68720b8
mariannesimon <mariannesimon95@gmail.com>
2017-12-08 11:42:59 +0100

    modif return start_transcribe

--- class_individu.py
+++ class_individu.py
@@ -5,20 +5,20 @@ class individu():
 		self.tss = tss
 		self.gff_df_raw=gff_df
 		self.prot = prot
-		
+
 		self.TTS_pos = (tts['TTS_pos'].values/DELTA_X).astype(int)
 		self.newTTS_pos = np.copy(self.TTS_pos)
-		
+
 		self.genome_size = genome_size
 		self.genome = int(genome_size/DELTA_X)
 		self.newgenome = genome
-		
+
 		self.TSS_pos = (tss['TSS_pos'].values/DELTA_X).astype(int)
 		self.newTSS_pos = np.copy(self.TTS_pos)
-		
+
 		self.Barr_fix = (prot['prot_pos'].values/DELTA_X).astype(int)
 		self.newBarr_fix = np.copy(Barr_fix)
-		
+
 		self.strands = self.str2num(gff_df['strand'].values)
 		self.newstrands = np.copy(strands)
 
@@ -31,15 +31,14 @@ class individu():
 		inv1=np.random.randint(0,self.genome)
 		while set(np.ndarray.tolist(np.where(inv1>self.TSS_pos)[0])).intersection(np.ndarray.tolist(np.where(inv1<self.TTS_pos)[0])): # test if inv1 belongs to non-coding part
 			inv1=np.random.randint(0,self.genome)
-	
+
 		inv2=np.random.randint(0,self.genome)
 		while set(np.ndarray.tolist(np.where(inv2>self.TSS_pos)[0])).intersection(np.ndarray.tolist(np.where(inv2<self.TTS_pos)[0])):  # test if inv2 belongs to non-coding part
 			inv2=np.random.randint(0,self.genome)
-	
+
 		if inv1>inv2:
 			S=set(np.ndarray.tolist(np.where(inv1<self.TSS_pos)[0])).union(np.ndarray.tolist(np.where(inv2>self.TTS_pos)[0]))
 		else:
 			S=set(np.ndarray.tolist(np.where(inv1<self.TSS_pos)[0])).intersection(np.ndarray.tolist(np.where(inv2>self.TTS_pos)[0]))
-	
-		self.strands[list(S)]=-self.strands[list(S)]
 
+		self.strands[list(S)]=-self.strands[list(S)]
